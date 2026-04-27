17447b54fe2c60051b5540999a627c13964422b5
Abdul-Ahad-hub <abdulahadtayyab07@gmail.com>
2026-04-27 13:53:55 +0500

    Deploayment 2.0

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ const startServer = async () => {
   app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
     credentials: true
   }));
 
